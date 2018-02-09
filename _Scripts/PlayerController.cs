14641bbb842b4d73f0eead7007d1d9d700507f1e
atamri <36233470+atamri@users.noreply.github.com>
2018-02-09 12:45:35 -0500

    creating test
    testing

---
+++ _Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody2D rb;
+
+
+	void FixedUpdate () {
+
+		rb = GetComponent <Rigidbody2D> ();
+
+		float x = Input.GetAxis ("Horizontal");
+		float y = Input.GetAxis ("Vertical");
+
+	
+		rb.position = rb.position + new Vector2 (x, 0f);
+
+	}
+}
